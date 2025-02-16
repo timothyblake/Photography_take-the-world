aee09e93d0b10e1d4c1de30474182944be7aa9d7
timothyblake <timothynblake@gmail.com>
2025-02-16 10:13:04 +0000

    Updated fav icons & logos

--- header.php
+++ header.php
@@ -22,21 +22,12 @@
 
 
     <!--Apple Touch Icons-->
-    <link rel="apple-touch-icon" sizes="57x57" href="https://timothyblake.com/wp-content/themes/tb-compile/images/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="https://timothyblake.com/wp-content/themes/tb-compile/images/favicon/faviconapple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="https://timothyblake.com/wp-content/themes/tb-compile/images/favicon/faviconapple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="https://timothyblake.com/wp-content/themes/tb-compile/images/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="https://timothyblake.com/wp-content/themes/tb-compile/images/favicon/faviconapple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="https://timothyblake.com/wp-content/themes/tb-compile/images/favicon/faviconapple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="https://timothyblake.com/wp-content/themes/tb-compile/images/favicon/faviconapple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="https://timothyblake.com/wp-content/themes/tb-compile/images/favicon/favicon/faviconapple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="https://timothyblake.com/wp-content/themes/tb-compile/images/favicon/faviconapple-icon-180x180.png">
-
-    <!--Web Favicons-->
-    <link rel="icon" type="image/png" sizes="192x192"  href="https://timothyblake.com/wp-content/themes/tb-compile/images/favicon/faviconandroid-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="https://timothyblake.com/wp-content/themes/tb-compile/images/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="https://timothyblake.com/wp-content/themes/tb-compile/images/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="https://timothyblake.com/wp-content/themes/tb-compile/images/favicon/favicon-16x16.png">
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<meta name="apple-mobile-web-app-title" content="Timothy Blake" />
+	<link rel="manifest" href="/site.webmanifest" />
 
 	 <!--Fonts-->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
@@ -56,9 +47,8 @@
             <div>
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 					
-					<img src="https://timothyblake.com/wp-content/themes/tb-compile/img/timothy-blake-logo-black.svg" alt="Timothy Blake logo" width="50" class="light-mode-logo"/>
+					<img src="https://timothyblake.com/_astro/timothy-blake-logo-black.D9h9S3wC_ZVwjfD.svg" alt="Timothy Blake logo" width="50" class="light-mode-logo"/>
 
-					<img src="https://timothyblake.com/wp-content/themes/tb-compile/images/timothy-blake-logo-white.svg" alt="Timothy Blake logo" width="50" class="dark-mode-logo" class="py-3"/>
 				</a>
             </div>
 
